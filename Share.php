6272993c6614994b82ac4829e3e6c3d0b8547ff7
xuchao <85825770@qq.com>
2017-05-08 19:21:55 +0800

    update

---
+++ Share.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\share;
+
+use Yii;
+use yii\base\Widget;
+
+class Share extends Widget
+{
+
+}
