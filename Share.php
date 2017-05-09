6efa802ddfb137d0dc1ad64ea56d8114dacb9a02
xuchao <85825770@qq.com>
2017-05-09 10:29:34 +0800

    分享

--- Share.php
+++ Share.php
@@ -41,7 +41,19 @@ class Share extends Widget
     /**
      * @var array
      */
-    public $items;
+    public $items = [];
+
+    /**
+     * @var array the HTML attributes for the input tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $ulOptions = [];
+
+    /**
+     * @var array the HTML attributes for the input tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
 
     /**
      * @inheritdoc
@@ -52,18 +64,32 @@ class Share extends Widget
         if (!isset ($this->options ['id'])) {
             $this->options ['id'] = $this->getId();
         }
+        $this->options ['data'] = [
+            'message' => $this->message,
+            'summary' => $this->summary,
+            'picture' => $this->picture,
+            'url' => $this->url,
+        ];
     }
 
+    /**
+     * run
+     */
     public function run()
     {
-        echo Html::beginTag('div',$this->options);
+        echo Html::beginTag('div', $this->options);
 
-        echo Html::ul($this->items);
-
+        $items = '';
+        foreach ($this->items as $key => $value) {
+            $a = Html::a($value, 'javascript:void(0);', [
+                'class' => "entypo-{$key} icon-sn-{$key} share",
+                'data' => ['toggle' => 'tooltip', 'placement' => 'top', 'title' => '', 'original-title' => '分享至' . $value],
+            ]);
+            $items .= Html::tag('li', $a, ['data' => ['network' => $key]]);
+        }
+        echo Html::tag('ul',$items);
         echo Html::endTag('div');
-            echo Html::activeTextArea($this->model, $this->attribute, $this->options);
-
         ShareAsset::register($this->view);
-        $this->view->registerJs("jQuery('{$this->options['id']}').share();");
+        $this->view->registerJs("jQuery('#{$this->options['id']}').share();");
     }
 }
